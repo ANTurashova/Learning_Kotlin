d2ff699aa30001328fa126a2a2feeb9a017a0be1
AnnaTurashova <30anna30.08@gmail.com>
2020-11-03 12:24:08 +0300

    Add project Fractal

--- Polynom-GraphicsPanel/src/ru/anna/gui/graphics/Painter.kt
+++
@@ -1,8 +0,0 @@
-// Здесь описываем какие-то методы, которые мне будут нужны
-package ru.anna.gui.graphics
-
-import java.awt.Graphics
-
-interface Painter {
-    fun paint(g: Graphics?)  // В GraphicsPanel вызываем этот метод. Включаем здесь метод в интерфейс.
-}
